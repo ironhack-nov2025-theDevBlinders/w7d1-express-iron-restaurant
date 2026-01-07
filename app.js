25df200e9a189f624b53058a8ef5765c0ebf42ae
Luis Junco <38411763+luisjunco@users.noreply.github.com>
2026-01-07 10:14:43 +0100

    remove dead code from app.js

--- app.js
+++ app.js
@@ -17,30 +17,6 @@ app.use(express.static('public'));
 app.use(express.json());
 
 
-//
-// Syntax to create a route in Express:
-// - app.get(path, code)
-// - app.get("/contact", function(req, res, next) {})
-//
-
-//
-// Some methods to send an http response:
-// - res.send()
-// - res.sendFile()
-// - res.json()
-//
-
-
-// 
-// Example of custom middleware functions
-// 
-function sayHello(req, res, next) {
-    console.log("hello Express");
-    next()
-}
-
-app.use(sayHello)
-
 
 
 // GET /
